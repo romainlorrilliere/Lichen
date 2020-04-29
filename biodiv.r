63fddaefde4e423138216356109ea7897fdd31f1
Romain Lorrilliere <romainlorrilliere@gmail.com>
2020-04-29 13:59:30 +0200

    Update biodiv.r

--- biodiv.r
+++ biodiv.r
@@ -19,6 +19,7 @@ prepa <- function() {
     d_sp_sample <-  melt(d_sp_sample.w, id.vars=c("sample_id"))
     colnames(d_sp_sample)[2:3] <- c("sp","recouvr")
 
+
     d_sample <- inner_join(d_sample,d_sp_sample)
 
     d_sample$presence <- ifelse(d_sample$recouvr>0,1,0)
@@ -49,12 +50,12 @@ get_Q <- function(d,plot=TRUE) {
     }
 
     write.csv(dq,"output/sp_Q.csv",row.names=FALSE)
-    return(d_q)
+    return(dq)
 }
 
 
 
-get_VDL <- function(d,plot=TRUE) {
+get_IBLE <- function(d,plot=TRUE) {
 
     d_maille <- aggregate(recouvr~site_id + arbre_id + face_id + face + sp , d, mean)
     d_freq <- aggregate(presence~site_id + arbre_id + face_id + face + sp ,d,sum)
@@ -63,7 +64,37 @@ get_VDL <- function(d,plot=TRUE) {
     dd$prod <- dd$recouvr * dd$presence
 
     d_site_face <- aggregate(prod~site_id+face,dd,mean)
-    d_vdl <- aggregate(prod~site_id,d_site_face,sum)
+    d_ible <- aggregate(prod~site_id,d_site_face,sum)
+    colnames(d_ible)[2] <- "ible"
+
+
+
+    print(summary(d_ible))
+
+    if(plot) {
+        gg <- ggplot(data=d_ible,aes(x="",y=ible)) + geom_violin(draw_quantles=c(.25,.5,.75)) + geom_boxplot(notch=TRUE,width=.2, outlier.colour=NA)+ geom_jitter(width = 0.05,alpha=0.3)
+        gg <- gg + labs(x="",y="IBLE",title="Distribution du IBLE sur les sites",subtitle="Les 3 quantiles 0.25, 0.5, 0.75")
+
+        print(gg)
+        ggsave("output/site_ible.png",gg)
+    }
+
+    write.csv(d_ible,"output/site_ible.csv",row.names=FALSE)
+    return(d_ible)
+}
+
+
+
+get_VDL <- function(d,plot=TRUE) {
+### Louis-rose, S., 2012. Air quality - Biomonitoring with lichens - Assessing epiphytic lichen diversity.
+### LDV
+### each arbre sum freq toute espèces par face
+### mean par face pour la station
+### additionne les moyenne  -> LDV
+
+    d_freq_tree <- aggregate(presence~site_id + arbre_id ,d,sum)
+    d_freq_tree$freq <- d_freq_tree$presence
+    d_vdl <- aggregate(freq~site_id,d_freq_tree,mean)
     colnames(d_vdl)[2] <- "vdl"
 
     ## LDV
@@ -74,8 +105,8 @@ get_VDL <- function(d,plot=TRUE) {
     print(summary(d_vdl))
 
     if(plot) {
-        gg <- ggplot(data=d_vdl,aes(x="",y=vdl)) + geom_violin() + geom_boxplot(notch=TRUE,width=.2, outlier.colour=NA)+ geom_jitter(width = 0.05,alpha=0.3)
-        gg <- gg + labs(x="",y="VDL",title="Distribution du VDL (IBLE) sur les sites",subtitle="Les 3 quantiles 0.25, 0.5, 0.75")
+        gg <- ggplot(data=d_vdl,aes(x="",y=vdl)) + geom_violin(draw_quantles=c(.25,.5,.75)) + geom_boxplot(notch=TRUE,width=.2, outlier.colour=NA)+ geom_jitter(width = 0.05,alpha=0.3)
+        gg <- gg + labs(x="",y="VDL",title="Distribution du VDL sur les sites",subtitle="Les 3 quantiles 0.25, 0.5, 0.75")
 
         print(gg)
         ggsave("output/site_vdl.png",gg)
@@ -87,19 +118,76 @@ get_VDL <- function(d,plot=TRUE) {
 
 
 
-get_ipa <- function(d,dq) {
+
+get_ipa <- function(d,dq,plot=TRUE) {
     ## IPA
     ## F transforme pour valeur entre 1..5
     ## somme des de somme par site de Q(sp)*F(sp)/10 -> IPA
 
-    d_site <- aggregate(presence~site_id+face+sp,d,sum)
-    d$freq1_5<- (d$abondance-1)/(5-1)
+    require(scales)
+    require(dplyr)
 
+    d_site <- aggregate(presence~site_id+sp,d,sum)
+    d_site$freq1_5 <-  scales::rescale(d_site$presence, to = c(1,5),from=c(0,100))
 
+    d_site$sp <- as.character(d_site$sp)
+    dq$sp <- as.character(dq$sp)
+
+    d_site <- inner_join(d_site,dq)
+
+    d_site$iap_sp <- d_site$freq1_5 * d_site$q /10
+
+    d_ipa <- aggregate(iap_sp~site_id,d_site, sum)
+    colnames(d_ipa)[2] <- "ipa"
+
+   print(summary(d_ipa))
+
+    if(plot) {
+        gg <- ggplot(data=d_ipa,aes(x="",y=ipa)) + geom_violin(draw_quantiles=c(.25,.5,.75)) + geom_boxplot(notch=TRUE,width=.2, outlier.colour=NA)+ geom_jitter(width = 0.05,alpha=0.3)
+        gg <- gg + labs(x="",y="IPA",title="Distribution du IPA sur les sites",subtitle="Les 3 quantiles 0.25, 0.5, 0.75")
+        print(gg)
+        ggsave("output/site_ipa.png",gg)
+    }
+
+    write.csv(d_ipa,"output/site_ipa.csv",row.names=FALSE)
+    return(d_ipa)
 
 
 }
 
+
+
+get_lichen_indic <- function() {
+
+
+    d <- prepa()
+
+    d_q <- get_Q(d)
+    d_ible <- get_IBLE(d)
+    d_vdl <- get_VDL(d)
+    d_ipa <- get_ipa(d,d_q)
+
+
+    d_lichen <- inner_join(inner_join(d_ible,d_vdl),d_ipa)
+
+
+    library(psych)
+
+
+    pairs.panels(d_lichen[,-1],
+                 method = "pearson", # correlation method
+                 hist.col = "#00AFBB",
+                 density = TRUE,  # show density plots
+                 ellipses = TRUE # show correlation ellipses
+                 )
+
+
+
+    return(d_lichen)
+    write.csv(d_lichen,"outpout/indic_lichen.csv")
+
+
+}
 
 
 
